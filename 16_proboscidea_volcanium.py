8b6fa2c520b31c003d69b5abd18648e890f98bb3
André Larsson <andre.larsson@softhouse.se>
2022-12-20 23:40:50 +0100

    day 16 added code for part 2, answer off by one for example

--- 16_proboscidea_volcanium.py
+++ 16_proboscidea_volcanium.py
@@ -1,7 +1,8 @@
 # load the data
 from collections import defaultdict
-from copy import deepcopy
+from copy import deepcopy, copy
 from pprint import pprint
+from time import perf_counter
 
 import numpy as np
 
@@ -40,46 +41,38 @@ def get_distances(valve_dict, start_valve):
 
 pprint(init_valve_dict)
 
-def get_best_moves(valve_dict, start_pos, countdown):
+def get_moves(valve_dict, start_pos, countdown):
     distances, previous = get_distances(valve_dict, start_pos)
     valve_scores = list()
     for valve, dist in distances.items():
         if dist+1 > countdown:
             continue
 
-        # calculate total eventual pressure (TEP) if
-        # we go to a valve and open it
         flow_rate = valve_dict[valve]["flow_rate"]
-        TEP = (countdown - dist-1) * flow_rate
-        valve_scores.append((valve, TEP, dist+1, flow_rate))
+        if flow_rate > 0:
+            valve_scores.append((valve, dist, flow_rate))
 
-    # sort by TEP, then by distance
-    valve_scores.sort(key=lambda x: (x[1], x[2]), reverse=True)
     return valve_scores
 
-tp_list = list()
-
-def calculate_pressure(valve_dict, current_valve, valve_is_open, countdown, total_ppm):
+def calculate_pressure1(valve_dict, current_valve, valve_is_open, countdown=30, total_ppm=0):
     if countdown <= 0:
         return 0
 
-    valve_scores = get_best_moves(valve_dict, current_valve, countdown)
-    valve_scores = [x for x in valve_scores if x[1] != 0]  # many of the rooms have 0 flow rate
+    valve_list = get_moves(valve_dict, current_valve, countdown)
+    valve_list = [x for x in valve_list if not valve_is_open[x[0]]]
+
 
     total_pressure_list = list()
 
-    for valve_score in valve_scores:
-        best_valve, TEP, dist, flow_rate = valve_score
+    for valve in valve_list:
+        valve_name, dist, flow_rate = valve
 
-        if best_valve is not None and dist <= countdown and not valve_is_open[best_valve]:
-            new_valve_is_open = valve_is_open.copy()
-            new_valve_is_open[best_valve] = True
-            add_pressure = total_ppm * dist
-            total_pressure = calculate_pressure(valve_dict, best_valve, new_valve_is_open, countdown-dist,
-                                                total_ppm+flow_rate) + add_pressure
-            total_pressure_list.append(total_pressure)
-        else:
-            total_pressure_list.append(countdown * total_ppm)
+        new_valve_is_open = valve_is_open.copy()
+        new_valve_is_open[valve_name] = True
+
+        total_pressure = calculate_pressure1(valve_dict, valve_name, new_valve_is_open, countdown-dist-1,
+                                            total_ppm+flow_rate) + total_ppm * (dist+1)
+        total_pressure_list.append(total_pressure)
 
     if len(total_pressure_list) > 0:
         return max(total_pressure_list)
@@ -89,5 +82,172 @@ def calculate_pressure(valve_dict, current_valve, valve_is_open, countdown, tota
 # part a
 valve_dict = deepcopy(init_valve_dict)
 valve_is_open = defaultdict(lambda: False)
-total_pressure = calculate_pressure(valve_dict, "AA", valve_is_open, 30, 0)
+t0 = perf_counter()
+total_pressure = calculate_pressure1(valve_dict, "AA", valve_is_open)
 print(f"Total pressure: {total_pressure}")
+print(f"Time: {perf_counter() - t0}")
+
+
+class Agent:
+    def __init__(self, current_valve = "AA"):
+        self.current_valve = current_valve
+        self.target_valve = None
+        self.dist_to_target = 0
+        self.opening_valve = False
+        self.is_moving = False
+
+    def update_location(self):
+        if self.dist_to_target <= 0 and self.is_moving:
+            self.current_valve = self.target_valve
+            self.target_valve = None
+            self.is_moving = False
+            self.dist_to_target = 0
+            self.opening_valve = True
+
+    def set_target(self, target_valve, dist_to_target):
+        self.target_valve = target_valve
+        self.dist_to_target = dist_to_target
+        self.is_moving = True
+
+    def move_towards_target(self, steps=1):
+        self.dist_to_target = max(self.dist_to_target - steps, 0)
+        self.update_location()
+
+    def __repr__(self):
+        repr_str = vars(self)
+        return str(repr_str)
+
+def calculate_pressure2(valve_dict, a1, a2,
+                        valve_is_open, countdown=26, total_ppm=0, history=[]):
+
+    history = history.copy()
+    history.append((deepcopy(a1), deepcopy(a2), total_ppm, countdown))
+
+    if countdown <= 0:
+        return 0, history
+
+    new_total_ppm = total_ppm
+
+    if a1.opening_valve:
+        # a1 is opening a valve this turn
+        new_total_ppm += valve_dict[a1.current_valve]["flow_rate"] # flow rate increases
+        a1.opening_valve = False # reset opening valve
+        valve_list1 = list() # no moves for a1
+    elif a1.is_moving:
+        a1.move_towards_target(1)
+        valve_list1 = list()
+    else:
+        valve_list1 = get_moves(valve_dict, a1.current_valve, countdown)
+        valve_list1 = [x for x in valve_list1 if not valve_is_open[x[0]]]
+
+    if a2.opening_valve:
+        new_total_ppm += valve_dict[a2.current_valve]["flow_rate"]
+        a2.opening_valve = False
+        valve_list2 = list()
+    elif a2.is_moving:
+        a2.move_towards_target(1)
+        valve_list2 = list()
+    else:
+        valve_list2 = get_moves(valve_dict, a2.current_valve, countdown)
+        valve_list2 = [x for x in valve_list2 if not valve_is_open[x[0]]]
+
+
+    total_pressure_list = list()
+
+    if len(valve_list1) == 0 and len(valve_list2) == 0:
+        result = calculate_pressure2(valve_dict, deepcopy(a1), deepcopy(a2) , deepcopy(valve_is_open), countdown-1, new_total_ppm, history)
+        return result[0] + total_ppm, result[1]
+    elif len(valve_list2) == 0:
+        # find new target for agent 1, loop through all possibilities
+        for valve1 in valve_list1:
+            valve_name, dist, flow_rate = valve1
+            a1_new = deepcopy(a1)
+            a1_new.set_target(valve_name, dist)
+            a1_new.move_towards_target(1)
+            new_valve_is_open = deepcopy(valve_is_open)
+            new_valve_is_open[valve_name] = True
+
+            result = calculate_pressure2(valve_dict, a1_new, deepcopy(a2), new_valve_is_open, countdown-1, new_total_ppm, history)
+            total_pressure_list.append((result[0] + total_ppm, result[1]))
+    elif len(valve_list1) == 0:
+        # find new target for agent 2
+        for valve2 in valve_list2:
+            valve_name, dist, flow_rate = valve2
+            a2_new = deepcopy(a2)
+            a2_new.set_target(valve_name, dist)
+            a2_new.move_towards_target(1)
+
+            new_valve_is_open = deepcopy(valve_is_open)
+            new_valve_is_open[valve_name] = True
+
+            result = calculate_pressure2(valve_dict, deepcopy(a1), a2_new, new_valve_is_open, countdown-1, new_total_ppm, history)
+            total_pressure_list.append((result[0] + total_ppm, result[1]))
+    else:
+        # find new target for both agents
+        for i, valve1 in enumerate(valve_list1):
+            for j, valve2 in enumerate(valve_list2):
+                if i > j:
+                    continue
+
+                if i == j and (len(valve_list1) != 1 or len(valve_list2) != 1):
+                    continue
+
+                if i == j:
+                    # same valve, only one agent can move
+                    valve_name1, dist1, flow_rate1 = valve1
+
+                    a1_new = deepcopy(a1)
+
+                    a1_new.set_target(valve_name1, dist1)
+                    a1_new.move_towards_target(1)
+
+                    new_valve_is_open = deepcopy(valve_is_open)
+                    new_valve_is_open[valve_name1] = True
+
+                    result = calculate_pressure2(valve_dict, a1_new, deepcopy(a2), new_valve_is_open, countdown-1, new_total_ppm, history)
+
+                    total_pressure_list.append((result[0] + total_ppm, result[1]))
+                else:
+                    valve_name1, dist1, flow_rate1 = valve1
+                    valve_name2, dist2, flow_rate2 = valve2
+
+                    a1_new = deepcopy(a1)
+                    a2_new = deepcopy(a2)
+
+                    a1_new.set_target(valve_name1, dist1)
+                    a2_new.set_target(valve_name2, dist2)
+
+                    a1_new.move_towards_target(1)
+                    a2_new.move_towards_target(1)
+
+                    new_valve_is_open = deepcopy(valve_is_open)
+                    new_valve_is_open[valve_name1] = True
+                    new_valve_is_open[valve_name2] = True
+
+                    result = calculate_pressure2(valve_dict, a1_new, a2_new, new_valve_is_open, countdown-1, new_total_ppm, history)
+
+                    total_pressure_list.append((result[0] + total_ppm, result[1]))
+
+    if len(total_pressure_list) == 0:
+        return total_ppm, history
+    else:
+        max_pressure = max(total_pressure_list, key=lambda x: x[0])
+        for result in total_pressure_list:
+            if result[0] == max_pressure[0]:
+                return result
+
+valve_dict = deepcopy(init_valve_dict)
+valve_is_open = defaultdict(lambda: False)
+t0 = perf_counter()
+result = calculate_pressure2(valve_dict, Agent(), Agent(), valve_is_open, 26)
+print(result)
+print(f"Time: {perf_counter() - t0}")
+
+for i, d in enumerate(result[1]):
+    print(f"== Minute {i} ==")
+    print(f"{d[0]}")
+    print(f"{d[1]}")
+    print(f"Total pressure/minute: {d[2]}")
+    print(f"Countdown: {d[3]}")
+
+print(f"Total pressure: {result}")
